c7510438bb56dc0d27ba262ec7cf7dd5c286d3e1
strawberryskeleton <xradhi5685@gmail.com>
2026-07-18 02:04:19 +0530

    setup script for pomodoro +showing default timer

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+const pomodoro = document.getElementById('pomodoro-timer')
+const timers = document.getElementById('timer-display')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+const timerMessage = document.getElementById('timer-message')
+const btn = document.querySelector('.btn')
+
+const short = document.getElementById('short-timer')
+const long = document.getElementById('long-timer')
+const session = document.getElementById('pomodoro-session')
+const shortBreak = document.getElementById('short-break')
+const longBreak = document.getElementById('long-break')
+
+
+let currentTimer = null
+let myInterval = null
+
+function showDefaultTimer () {
+    pomodoro.style.display = "block"
+    short.style.display = "none"
+    long.style.display = "none"
+}
+
+showDefaultTimer()
+
+function hideAll () {
+    timers.forEach((timer) => {
+        timer.style.display = "none"
+    })
+}
